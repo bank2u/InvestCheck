a14117cda268deb8205070507cd65477917952cc
Siwat Jirawiwatpat <bank2u@yahoo.com>
2026-06-13 20:10:47 +0700

    chore: untrack compiled JS artifacts from src/

--- src/utils/getMaxProjectionYears.js
+++
@@ -1,6 +0,0 @@
-export function getMaxProjectionYears(userAge) {
-    const maxAge = 90;
-    const minYears = 5;
-    const maxYears = maxAge - userAge;
-    return Math.max(minYears, maxYears);
-}
